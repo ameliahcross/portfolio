6710a0f3e9b1c943737589c07dc477d5e0e81e79
Amelia Henriquez Cross <ameliahcross@gmail.com>
2023-12-11 20:26:11 -0400

    updated portfolio with real images

--- src/components/MasonryGallery.js
+++ src/components/MasonryGallery.js
@@ -1,44 +1,87 @@
-import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/MoreAbout.css";
 import "../styles/MasonryGallery.css";
+import React, {useState} from "react";
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import { FaArrowCircleLeft ,FaArrowCircleRight } from "react-icons/fa";
 import { IoMdCloseCircle } from "react-icons/io";
+import anxietin from '../images/anxietin.png';
+import ui_1 from '../images/location_app_ui_start.png';
+import ui_2 from '../images/location_app_ui_front.png';
+import ui_3 from '../images/perspective_ui.png';
+import anxiologica_cards from '../images/anxiologica_cards.png';
+import into_lettering from '../images/into_lettering.png';
+import card1 from '../images/miguel.png';
+import card2 from '../images/saleo.png';
+import brascaribe from '../images/brascaribe.png';
+import insumisas from '../images/insumisas.png';
+import senaletica from '../images/senaletica.png';
+import violet from '../images/violetcharm.png';
+import venaver from '../images/venaver_branding.png';
+import arcadia from '../images/socual_media.png';
 
 // Arreglo de imagenes
-const images = [
-  "https://picsum.photos/2000/3000?",
-  "https://picsum.photos/4000/3000?",
-  "https://picsum.photos/2000/1000?",
-  "https://picsum.photos/1300/1000?",
-  "https://picsum.photos/3000/1500?",
-  "https://picsum.photos/3000/2000?",
-]
 
 const images2 = [
-  { url: "https://picsum.photos/2000/3000?", 
-    pieFoto: "Este es el pie de foto de mi primer objeto de imagen"
+  {
+    url: anxietin,
+    pieFoto: "Este es el pie de foto de mi primer objeto de imagen",
   },
-  { url: "https://picsum.photos/4000/3000?", 
-    pieFoto: "Este es el pie de foto de mi segundo objeto de imagen"
+  {
+    url: ui_1,
+    pieFoto: "Este es el pie de foto de mi segundo objeto de imagen",
   },
-  { url: "https://picsum.photos/2000/1000?", 
-    pieFoto: "Este es el pie de foto de mi tercer objeto de imagen"
+  {
+    url: ui_2,
+    pieFoto: "Este es el pie de foto de mi tercer objeto de imagen",
   },
-  { url: "https://picsum.photos/1300/1000?", 
-    pieFoto: "Este es el pie de foto de mi cuarto objeto de imagen"
+  {
+    url: ui_3,
+    pieFoto: "Este es el pie de foto de mi cuarto objeto de imagen",
   },
-  { url: "https://picsum.photos/3000/2000?", 
-    pieFoto: "Este es el pie de foto de mi quinto objeto de imagen"
+  {
+    url: venaver,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
   },
-  { url: "https://picsum.photos/4000/3003?", 
-  pieFoto: "Este es el pie de foto de mi sexto objeto de imagen"
+  {
+    url: anxiologica_cards,
+    pieFoto: "Este es el pie de foto de mi quinto objeto de imagen",
   },
-  { url: "https://picsum.photos/2040/1000?", 
-    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen"
+  {
+    url: senaletica,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
   },
-]
+  {
+    url: insumisas,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
+  },
+  {
+    url: card1,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
+  },
+  {
+    url: into_lettering,
+    pieFoto: "Este es el pie de foto de mi sexto objeto de imagen",
+  },
+  {
+    url: violet,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
+  },
+  {
+    url: arcadia,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
+  },
+
+  {
+    url: brascaribe,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
+  },
+  {
+    url: card2,
+    pieFoto: "Este es el pie de foto de mi septimo objeto de imagen",
+  },
+ 
+];
 
 function MasonryGallery() {
 
@@ -54,10 +97,6 @@ function MasonryGallery() {
 
   const closeImage = () => {
     setOpenImage(null);
-  };
-
-  const getIndex = () => {
-    console.log(openImage)
   };
 
   const previousImage = (i) => {
@@ -77,7 +116,7 @@ function MasonryGallery() {
 
   return (
     <>
-      {openImage && (
+      {openImage !== null && (
         <div className="fullscreen-image row">
           <div className="close-button col-1" onClick={closeImage}><IoMdCloseCircle /></div>
           <img src={images2[openImage].url} alt={images2[openImage].pieFoto} className="fullscreen-img" />
@@ -92,7 +131,7 @@ function MasonryGallery() {
       <div style={{ paddingTop: "10px" }}>
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3}}>
         <Masonry gutter="10px">
-            {images2.map((image, i) => (
+            {images2.map((image,i) => (
                 <img
                 key={i}
                 src={image.url}
